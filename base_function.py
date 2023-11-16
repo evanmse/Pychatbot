8c6357c45c34a85fab54f1836704bf7da5329252
evanmse <evanmse4@gmail.com>
2023-11-16 15:56:32 +0100

    push lowerClean

--- base_function.py
+++ base_function.py
@@ -18,14 +18,31 @@ def extractNameFile(nameFile):  #Function that extracts the name of the presiden
     else:
         return None #If he doesn't find the group with the pattern
 
-def assocNamePres(namePresident): #Function that associate to a president name, its first name
+def assocNamePres(namePresident):
     dictPres = {'Chirac': 'Jacques', 'Giscard dEstaing': 'Valéry', 'Hollande': 'François', 'Macron': 'Emmanuel', 'Mitterrand': 'François','Sarkozy': 'Nicolas' }
     return dictPres[namePresident]
 
-def lowerClean():
+def lowerClean(inputFile): # Input name of file to go upper
+    # Path of the files and directory
+    path_input_file = "./speeches/{}".format(inputFile)
+    path_cleaned_dir = "./cleaned"
+    path_cleaned_file = "./cleaned/{}".format(inputFile)
+    # Test if the file exists 
+    if not os.path.exists(path_cleaned_dir):
+        os.mkdir(path_cleaned_dir)
+    # Copy the content from speeches to cleaned with change letter lowercase
+    with open(path_input_file, 'r', encoding='utf-8') as file, open(path_cleaned_file, 'w', encoding='utf-8') as output_file:
+            content = file.read()
+            content_lower = content.lower()
+            output_file.write(content_lower)
+    # Security close of file
+    file.close()
+    output_file.close()
     return
 
 def clearFile():
+
+
     return
 
 def list_namePres():  #Function that displays the list of the names of the presidents without duplications(doublons)
