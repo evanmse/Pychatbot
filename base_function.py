5dad934c0c9887709cf09ed285a1430eef5a0348
evanmse <evanmse4@gmail.com>
2023-11-18 13:27:44 +0100

    Update : - Function : cleanText, path_cleaned_file, path_speeches_file, listNamePres - Clear code of base_function.py - Delete of librairies re and string to respect the guidline

--- base_function.py
+++ base_function.py
@@ -1,59 +1,89 @@
 import os
-import re
 
-DIRECTORY = "./speeches"
 
 
 def list_of_files(directory, extension):  # Function that gives the list of files, Example : /speeches
     files_names = []
+
     for filename in os.listdir(directory):
         if filename.endswith(extension):
             files_names.append(filename)
+
     return files_names
 
-
 def extractNameFile(nameFile):  # Function that extracts the name of the president in the name of a certain file
-    match = re.match(r"Nomination_([a-zA-Z\s]+)\d*.txt", nameFile)  # Pattern to find in the name of files
-    if match:
-        return match.group(1)  # We just want to keep the groupe [Name of president]
-    else:
-        return None  # If he doesn't find the group with the pattern
+    namePres = "" # Variable for the result
 
+    nameSplit = nameFile.split("_")[1] # Split the name of the file and keep group 1 [Nomination_][[nom d’un president][numéro].txt]
+    nameSplit = nameSplit.split(".")[0] # Split the name of group 1 and keep before the dot group [[nom d’un president][numéro]][.txt]
 
+    for char in nameSplit: # Function that test in nameSplit if he has a number
+        if char.isspace():
+            namePres += " "
+        elif char.isalpha():
+            namePres += char
 
-def assocNamePres(namePresident):  # Function that associate to a president name, its first name
-    dictPres = {'Chirac': 'Jacques', 'Giscard dEstaing': 'Valéry', 'Hollande': 'François', 'Macron': 'Emmanuel',
-                'Mitterrand': 'François', 'Sarkozy': 'Nicolas'}
-    return dictPres[namePresident]
+    return namePres
 
-def assocNamePres(namePresident):
+def listNamePres(inputFile, extension):  #Function that displays the list of the names of the presidents without duplications(doublons)
+    list_name_pres = []
+
+    for name_file in list_of_files(inputFile, extension):
+        list_name_pres.append(extractNameFile(name_file))
+
+    return list(set(list_name_pres))
+
+def assocNamePres(namePresident): # Function that associate to a president name, its first name
     dictPres = {'Chirac': 'Jacques', 'Giscard dEstaing': 'Valéry', 'Hollande': 'François', 'Macron': 'Emmanuel', 'Mitterrand': 'François','Sarkozy': 'Nicolas' }
     return dictPres[namePresident]
 
 def lowerClean(inputFile): # Input name of file to go upper
-    # Path of the files and directory
-    path_input_file = "./speeches/{}".format(inputFile)
-    path_cleaned_dir = "./cleaned"
-    path_cleaned_file = "./cleaned/{}".format(inputFile)
-    # Test if the file exists 
+    # Test if the file exists
+    path_cleaned_dir = "./cleaned" 
     if not os.path.exists(path_cleaned_dir):
         os.mkdir(path_cleaned_dir)
-    # Copy the content from speeches to cleaned with change letter lowercase
-    with open(path_input_file, 'r', encoding='utf-8') as file, open(path_cleaned_file, 'w', encoding='utf-8') as output_file:
-            content = file.read()
-            content_lower = content.lower()
-            output_file.write(content_lower)
+
+    # Copy the content from speeches to cleaned with change letter in lowercase
+    with open(path_speeches_file(inputFile), 'r', encoding='utf-8') as file, open(path_cleaned_file(inputFile), 'w', encoding='utf-8') as output_file:
+        content = file.read()
+        output_file.write(content.lower()) # Writes the contents of a file in speeches to a file in cleaned in lowercase
+
     # Security close of file
     file.close()
     output_file.close()
-    return
-
-
-def clearFile():
 
     return
-  
-def list_namePres():  #Function that displays the list of the names of the presidents without duplications(doublons)
-    list_file = list_of_files(DIRECTORY, "txt")
-    president_names = [extractNameFile(filename) for filename in list_file]
-    return list(set(president_names))
+
+def clearFile(inputFile): #Main function for clean File
+    path_file = path_cleaned_file(inputFile)
+
+    with open(path_file, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    clearText = cleanText(text) #Call the sub-function for clean the text
+
+    with open(path_file, 'w', encoding='utf-8') as file:
+        file.write(clearText) # Write the clean text
+
+    return 
+
+def cleanText(text): #Sub-Function that clean a text
+    dicReplace = {'é':'e', 'à':'a', 'è':'e', 'ç':'c', 'ê': 'e', 'ë': 'e', 'ù':'u', 'â':'a'} # Dictionary of values to be changed
+    punctuation = [',',';','-','!','?','\'','.',':','"','`'] # List of punctuation to be change
+
+    for character in punctuation: # Replace of ponctuation
+        text = text.replace(character,' ')
+
+    for keys, value in dicReplace.items(): #Replace with dicReplace
+        text = text.replace(keys, value)
+
+    return text
+
+def path_cleaned_file(inputFile): #Function that give to a cleaned file its path
+    path_cleaned_file = "./cleaned/{}".format(inputFile)
+    return path_cleaned_file
+
+def path_speeches_file(inputFile): #Function that give to a speeches file its path
+    path_input_file = "./speeches/{}".format(inputFile)
+    return path_input_file
+
--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from base_function import *
-from tf_idf_functions import *
+from tf_idf_function import *
 
 if __name__ == "__main__":
+        # Main variables
         directory = "./speeches"
         files_names = list_of_files(directory, "txt")
 
         # Test for function extractNameFile :
-        president_names = [extractNameFile(filename) for filename in files_names] #List of president
+        print("### Test for function extractNameFile ### ")
+        president_names = []
+        for filename in files_names:
+                president_names.append(extractNameFile(filename))  #List of president
         print("Président names:", president_names) #Print of the list
 
 
         # Test for assocNamePres :
+        print("### Test for assocNamePres ### ")
         print(assocNamePres(president_names[3]))
 
-        #Test for list_namePres :
-        print(list_namePres())
+        #Test for listNamePres :
+        print("### Test for listNamePres ### ")
+        print(listNamePres(directory, "txt"))
 
         #Test for score_TF
-        with open('./speeches/Nomination_Chirac1.txt', 'r', encoding='utf-8') as file:
-                file_content = file.read()
-                print(score_TF(file_content))
+        #print("### Test for score_TF ### ")
+        #with open('./speeches/Nomination_Chirac1.txt', 'r', encoding='utf-8') as file:
+        #        file_content = file.read()
+        #        print(score_TF(file_content))
 
         # Test for lowerClean :
+        print("### Test for function lowerClean ### ")
         for name in list_of_files(directory, "txt"):
                 lowerClean(name)
 
+        # Test for clearFile :
+        print("### Test for function clearFile ### ")
+        for name in list_of_files("./cleaned", "txt"):
+                clearFile(name)
