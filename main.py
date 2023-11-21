a73f053d4119926d7767154c436e1c734689f3bb
Blemkeid <busithomas.loesch9+123@gmail.com>
2023-11-21 19:07:47 +0100

    I finished the score_TF_IDF function and tested it on main

--- main.py
+++ main.py
@@ -2,42 +2,45 @@ from base_function import *
 from tf_idf_function import *
 
 if __name__ == "__main__":
-        # Main variables
-        directory = "./speeches"
-        files_names = list_of_files(directory, "txt")
+    # Main variables
+    directory = "./speeches"
+    files_names = list_of_files(directory, "txt")
 
-        # Test for function extractNameFile :
-        print("\n### Test for function extractNameFile ### ")
-        president_names = []
-        for filename in files_names:
-                president_names.append(extractNameFile(filename))  #List of president
-        print("Président names:", president_names) #Print of the list
+    # Test for function extractNameFile :
+    print("\n### Test for function extractNameFile ### ")
+    president_names = []
+    for filename in files_names:
+        president_names.append(extractNameFile(filename))  # List of president
+    print("Président names:", president_names)  # Print of the list
 
+    # Test for assocNamePres :
+    print("\n### Test for assocNamePres ### ")
+    print(assocNamePres(president_names[3]))
 
-        # Test for assocNamePres :
-        print("\n### Test for assocNamePres ### ")
-        print(assocNamePres(president_names[3]))
+    # Test for listNamePres :
+    print("\n### Test for listNamePres ### ")
+    print(listNamePres(directory, "txt"))
 
-        #Test for listNamePres :
-        print("\n### Test for listNamePres ### ")
-        print(listNamePres(directory, "txt"))
+    # Test for score_TF
+    print("\n### Test for score_TF ### ")
+    with open('./speeches/Nomination_Chirac1.txt', 'r', encoding='utf-8') as file:
+        file_content = file.read()
+        print(score_TF(file_content))
 
-        #Test for score_TF
-        print("\n### Test for score_TF ### ")
-        with open('./speeches/Nomination_Chirac1.txt', 'r', encoding='utf-8') as file:
-              file_content = file.read()
-              print(score_TF(file_content))
+    # Test for lowerClean :
+    print("\n### Test for function lowerClean ### ")
+    for name in list_of_files(directory, "txt"):
+        lowerClean(name)
 
-        # Test for lowerClean :
-        print("\n### Test for function lowerClean ### ")
-        for name in list_of_files(directory, "txt"):
-                lowerClean(name)
+    # Test for clearFile :
+    print("\n### Test for function clearFile ### ")
+    for name in list_of_files("./cleaned", "txt"):
+        clearFile(name)
 
-        # Test for clearFile :
-        print("\n### Test for function clearFile ### ")
-        for name in list_of_files("./cleaned", "txt"):
-                clearFile(name)
+    # Test for score_IDF :
+    print("\n### Test for function score_IDF ### ")
+    print(score_IDF('./speeches'))
 
-        # Test for score_IDF :
-        print("\n### Test for function score_IDF ### ")
-        print(score_IDF('./speeches'))
+    # Test for score_TF_IDF :
+    print("\n### Test for function score_TF_IDF ### ")
+    print(score_TF_IDF('Nomination_Mitterrand1.txt', 'maire'))
--- tf_idf_function.py
+++ tf_idf_function.py
@@ -50,15 +50,15 @@ def score_IDF(directory):
     mydict = {}
     dictionnary_IDF = {}
 
-    for file in files_name:         #Go through each file in the directory
-        full_path = os.path.join('./speeches', file)    #Put in a variable the path of the file
+    for file in files_name:  # Go through each file in the directory
+        full_path = os.path.join('./speeches', file)  # Put in a variable the path of the file
 
         with open(full_path, 'r', encoding='utf-8') as f:
             content = f.read().lower()
             dictionnary_file = score_TF(content)
-            mylist = list(set(mylist + list(dictionnary_file.keys())))  #List containing all the words of all the files
+            mylist = list(set(mylist + list(dictionnary_file.keys())))  # List containing all the words of all the files
 
-    for element in mylist:      #Creation of the dictionnary containing all the elements of mylist as keys
+    for element in mylist:  # Creation of the dictionnary containing all the elements of mylist as keys
         mydict[element] = 0
 
     for document in files_name:
@@ -68,14 +68,27 @@ def score_IDF(directory):
             content = f2.read().lower()
             content_dictionnary = score_TF(content)
 
-            for word in mylist:     #Add 1 to mydict[word] if the word is in the .txt file
+            for word in mylist:  # Add 1 to mydict[word] if the word is in the .txt file
                 if word in list(content_dictionnary.keys()):
                     mydict[word] += 1
 
-    for key in list(mydict.keys()):             #Calculate the log of the inversed of the proportion of documents containing a certain word
+    for key in list(
+            mydict.keys()):  # Calculate the log of the inversed of the proportion of documents containing a certain word
         number_document = len(list_of_files('./speeches', '.txt'))
         proportion_document_containing_word = mydict[key] / number_document
-        inversed_proportion_document_containing_word = 1/proportion_document_containing_word
+        inversed_proportion_document_containing_word = 1 / proportion_document_containing_word
         dictionnary_IDF[key] = math.log(inversed_proportion_document_containing_word)
 
     return dictionnary_IDF
+
+
+def score_TF_IDF(document, word):  # Return the score TF-IDF of a certain word in a certain document
+    full_path = os.path.join('./speeches', document)
+    word = word.lower()
+
+    with open(full_path, 'r', encoding='utf-8') as f:
+        content = f.read().lower()
+        dictionnary_scoreTF_word = score_TF(content)
+        dictionnary_scoreIDF_word = score_IDF('./speeches')
+
+        return dictionnary_scoreTF_word[word] * dictionnary_scoreIDF_word[word]
