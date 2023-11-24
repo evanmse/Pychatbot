a7c6faa2d7c2af2c812cfbc58b8843de0a4f08dc
Tobymaru <150368953+Blemkeid@users.noreply.github.com>
2023-11-24 15:46:39 +0100

    I optimized the score_TF_IDF function and developed the first feature

--- func_function.py
+++ func_function.py
@@ -1,11 +1,27 @@
-from base_function import *
-from tf_idf_function import *
+from tf_idf_function import score_TF, score_TF_IDF
+from base_function import path_speeches_file
+import os
 
-def min_word_file_TD_IDF(file): # Functionality that give the word that not important in file, TD-IDF = 0
-    print("In development")
-    return
+def min_word_file_TD_IDF(): # Functionality that give the non important word, TD-IDF = 0
+    mylist = []
 
-def min_word_file_TD_IDF(file): # Functionality that give the word that most important in file, TD-IDF near of 1
+    files_name = os.listdir('./speeches')
+
+    for file in files_name:
+        print(file)
+        full_path = path_speeches_file(file)
+
+        with open(full_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+            list_of_word = list(score_TF(content).keys())
+
+        for i in list_of_word:
+            if score_TF_IDF(file, i) == 0:
+                mylist.append(i)
+
+    return mylist
+
+def max_word_file_TD_IDF(file): # Functionality that give the word(s) with the highest score TD_IDF
     print("In development")
     return
 
@@ -32,3 +48,5 @@ def talking_nation(): # Functionality that give who talks about climate
 def all_word_president(): # Functionality words all presidents have spoken without the unimportant words
     print("In development")
     return
+
+print(min_word_file_TD_IDF())
